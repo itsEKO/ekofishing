69728c6f4381c258376d15e47f4c3776677c8196
itsEKO <91019944+itsEKO@users.noreply.github.com>
2024-05-12 21:00:21 -0700

    1.0 version of plugin; fuctioning as intended

--- src/main/java/org/imeko/ekofishing/ekofishing.java
+++ src/main/java/org/imeko/ekofishing/ekofishing.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -33,20 +34,28 @@ public class ekofishing extends JavaPlugin implements Listener {
             for (ItemStack item : customLoot) {
                 Item drop = player.getWorld().dropItem(player.getLocation(), item);
                 drop.setPickupDelay(0);
+                event.getCaught().remove();
             }
         }
     }
 
     private List<ItemStack> getCustomFishingLoot() {
         List<ItemStack> loot = new ArrayList<>();
-        // Add custom items to the loot list
-        loot.add(getRandomItemStack(Material.DIAMOND, 1, 1));
-        loot.add(getRandomItemStack(Material.EMERALD, 1, 1));
-        loot.add(getRandomItemStack(Material.GOLD_INGOT, 1, 3));
-        // Add cores to the list
-        loot.add(getCoreItemStack("common"));
-        loot.add(getCoreItemStack("rare"));
-        loot.add(getCoreItemStack("epic"));
+        float lootChance = random.nextFloat();
+        // custom loot table
+        if (lootChance > 0.1 & lootChance < 0.15 ){
+            loot.add(getRandomItemStack(Material.DIAMOND, 1, 1));
+        } else if (lootChance > 0.15 & lootChance < 0.22) {
+            loot.add(getRandomItemStack(Material.EMERALD, 1, 1));
+        } else if (lootChance > 0.22 & lootChance < 0.32) {
+            loot.add(getRandomItemStack(Material.GOLD_INGOT, 1, 3));
+        } else if (lootChance > 0.32 & lootChance < 0.37) {
+            loot.add(getCoreItemStack("common"));
+        } else if (lootChance > 0.37 & lootChance < 0.4) {
+            loot.add(getCoreItemStack("rare"));
+        } else if (lootChance > 0.4 & lootChance < 0.41) {
+            loot.add(getCoreItemStack("epic"));
+        }
         return loot;
     }
 
@@ -56,22 +65,26 @@ public class ekofishing extends JavaPlugin implements Listener {
     }
 
     private ItemStack getCoreItemStack(String itemName){
-        ItemStack itemStack = new ItemStack(Material.CLOCK);
-        itemStack.getItemMeta().setLore(Collections.singletonList(ChatColor.GRAY + "Right Click to open"));
-        switch (itemName){
-            case "common":
-                itemStack.getItemMeta().setDisplayName(ChatColor.GRAY + "[" + ChatColor.YELLOW + "COMMON CORE" + ChatColor.GRAY + "]");
-                break;
+        ItemStack core = new ItemStack(Material.CLOCK);
+        ItemMeta coreMeta = core.getItemMeta();
+        if ( coreMeta != null ) {
+            coreMeta.setLore(Collections.singletonList(ChatColor.GRAY + "Right Click to open"));
+            switch (itemName) {
+                case "common":
+                    coreMeta.setDisplayName(ChatColor.GRAY + "[" + ChatColor.YELLOW + "COMMON CORE" + ChatColor.GRAY + "]");
+                    break;
 
-            case "rare":
-                itemStack.getItemMeta().setDisplayName(ChatColor.GRAY + "[" + ChatColor.AQUA + "RARE CORE" + ChatColor.GRAY + "]");
-                break;
+                case "rare":
+                    coreMeta.setDisplayName(ChatColor.GRAY + "[" + ChatColor.AQUA + "RARE CORE" + ChatColor.GRAY + "]");
+                    break;
 
-            case "epic":
-                itemStack.getItemMeta().setDisplayName(ChatColor.GRAY + "[" + ChatColor.DARK_PURPLE + "EPIC CORE" + ChatColor.GRAY + "]");
+                case "epic":
+                    coreMeta.setDisplayName(ChatColor.GRAY + "[" + ChatColor.DARK_PURPLE + "EPIC CORE" + ChatColor.GRAY + "]");
+            }
+            core.setItemMeta(coreMeta);
         }
 
-        return itemStack;
+        return core;
 
     }
 }
